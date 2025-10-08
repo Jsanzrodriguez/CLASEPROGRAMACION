fcf959fadd919f1759157db5297c7569cc99a5f8
Jsanzrodriguez <jsanzrodriguez9@gmail.com>
2025-10-08 11:03:41 +0200

    update

--- Tema 1/Proyectoinicial/src/Main.java
+++ Tema 1/Proyectoinicial/src/Main.java
@@ -4,5 +4,11 @@ public class Main {
         System.out.println("Hola esta es la segunda linea que hago");
         System.out.println("Esta es la tercera linea impresa");
 
+        System.out.print("Este es mi segunda vez haciendo un metodo");}
+
     }
-}
+
+
+
+
+
