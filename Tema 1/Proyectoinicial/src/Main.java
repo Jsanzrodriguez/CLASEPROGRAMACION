6a5e3050e029c56f95bb28b9678920e371b59e03
Jsanzrodriguez <jsanzrodriguez9@gmail.com>
2025-10-08 11:08:38 +0200

    update

--- Tema 1/Proyectoinicial/src/Main.java
+++ Tema 1/Proyectoinicial/src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hola este es mi primer proyecto");
         System.out.println("Hola esta es la segunda linea que hago");
         System.out.println("Esta es la tercera linea impresa");
 
-        System.out.print("Este es mi segunda vez haciendo un metodo");}
-
+        System.out.print("Este es mi segunda vez haciendo un metodo");
     }
+
+}
 
 
 
