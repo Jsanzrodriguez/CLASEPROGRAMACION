64bb45db7406013346eb391686b1b0d2e07a0425
Jsanzrodriguez <jsanzrodriguez9@gmail.com>
2025-10-09 10:43:47 +0200

    update

--- Tema 1/Proyectoinicial/src/Main.java
+++ Tema 1/Proyectoinicial/src/Main.java
@@ -1,14 +1,37 @@
 public class Main {
     public static void main(String[] args) {
+
+        String nombreCompleto= "JorgeSanz Rodriguez"; //Son palabras y van entre comillas dobles
+        nombreCompleto= "Jorge.S.R";
+        int edad= 23;
+        edad= edad +1;
+        float altura= 1.85f;
+        double peso= 100.002548;
+        char letraCarnet= 'B';
+        boolean experiencia= true;
+        System.out.println("Mi nombre es "+nombreCompleto);
+        System.out.println("Mi edad es "+edad);
+        System.out.println("Mi altura es "+altura);
+        System.out.println("Mi peso es "+peso);
+        System.out.println("Mi letra del carnet es "+letraCarnet);
+        System.out.println("Tengo experiencia "+experiencia);
         System.out.println("Hola este es mi primer proyecto");
         System.out.println("Hola esta es la segunda linea que hago");
         System.out.println("Esta es la tercera linea impresa");
-
         System.out.print("Este es mi segunda vez haciendo un metodo");
     }
 
+// este comentario es de solo una linea  y se puede poner donde quiera a modo comentario para mi
+/*
+Este comentario es para poder hacer aclaraciones si necesito
+mas de una linea
+ */
+
+    // TODO este comentario es lo que me queda por hacer
 
 }
+
+
 
 
 
