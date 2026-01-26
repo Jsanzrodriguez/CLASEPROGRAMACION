af1d65cecdccf0bb5e69e97130528c26e1abf507
Jsanzrodriguez <jsanzrodriguez9@gmail.com>
2026-01-26 18:37:00 +0100

    update

---
+++ Tema 5/GestorEscolarHerencia/src/Main.java
@@ -0,0 +1,71 @@
+import model.Alumno;
+import model.Persona;
+import model.Profesor;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenido a mi gestor escolar");
+        /*Persona Jorge = new Persona("Jorge","Sanz");
+        Jorge.saludar();
+        Jorge.mostrarDatos();*/
+        Alumno alumno1 = new Alumno("Jorge1", "Sanz", 1234, "DAM");
+        Alumno alumno2 = new Alumno("Jorge2", "Sanz", 1234, "DAM");
+        Alumno alumno3 = new Alumno("Jorge3", "Sanz", 1234, "DAM");
+        Alumno alumno4 = new Alumno("Jorge4", "Sanz", 1234, "DAM");
+        Profesor profesor1 = new Profesor("Maria1", "Lopez",30000,6);
+        Profesor profesor2 = new Profesor("Maria2", "Lopez",30000,6);
+        Profesor profesor3 = new Profesor("Maria3", "Lopez",30000,6);
+        Profesor profesor4 = new Profesor("Maria4", "Lopez",30000,6);
+
+        ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+        listaAlumnos.add(alumno1);
+        listaAlumnos.add(alumno2);
+        listaAlumnos.add(alumno3);
+        listaAlumnos.add(alumno4);
+        ArrayList<Profesor> listaProfesores = new ArrayList<>();
+        listaProfesores.add(profesor1);
+        listaProfesores.add(profesor2);
+        listaProfesores.add(profesor3);
+        listaProfesores.add(profesor4);
+        ArrayList<Persona> listaPersonas = new ArrayList<>();
+        listaPersonas.add(alumno1);
+        listaPersonas.add(alumno2);
+        listaPersonas.add(alumno3);
+        listaPersonas.add(alumno4);
+        listaPersonas.add(profesor1);
+        listaPersonas.add(profesor2);
+        listaPersonas.add(profesor3);
+        listaPersonas.add(profesor4);
+        for (Persona persona : listaPersonas) {
+            persona.saludar();
+            persona.mostrarDatos();
+
+            if (persona instanceof Alumno){
+                ((Alumno) persona).realizarExamen();
+            } else if (persona instanceof Profesor){
+                ((Profesor) persona).corregirExamen();
+            }
+            //((Profesor)persona).corregirExamen();
+            //((Alumno)persona).realizarExamen();
+        }
+
+        /*profesor.corregirExamen();
+        profesor.saludar();
+        alumno.saludar();
+        //profesor.saludar();
+        //alumno.saludar();
+        /*System.out.println("Indica el nombre del alumno");
+        alumno.setNombre("Jorge");
+        alumno.setApellido("Sanz");
+        alumno.setCurso("DAM");
+        alumno.setnMatricula(1234);*/
+       // alumno.mostrarDatos();
+       // alumno.realizarExamen();
+        // nombre =  apellido =
+    }
+}
---
+++ Tema 5/GestorEscolarHerencia/src/model/Alumno.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.sql.SQLOutput;
+
+public class Alumno extends Persona {
+
+    private int nMatricula;
+
+    private String curso;
+
+    public Alumno(){}
+    public Alumno(String nombre, String apellido, int nMatricula){
+        //llama al constructor de la superclase
+        super(nombre,apellido);
+        this.nMatricula = nMatricula;
+    }
+
+    public Alumno(String nombre, String apellido, int nMatricula, String curso){
+        //llama al constructor de la superclase
+        super(nombre, apellido);
+        this.nMatricula = nMatricula;
+        this.curso = curso;
+    }
+
+    @Override
+    public void saludar(){
+        System.out.println("Soy un alumno y voy a aprender un monton");
+    }
+
+    @Override
+    public void mostrarDatos() {
+        System.out.println("nMatricula = " + nMatricula);
+        System.out.println("curso = " + curso);
+        super.mostrarDatos();
+    }
+    public void realizarExamen(){
+        System.out.println("Vamos a realizar el examen");
+        int nota = (int) (Math.random()*11);
+        System.out.println("La nota del examen ha sido "+nota);
+    }
+
+    public int getnMatricula() {
+        return nMatricula;
+    }
+
+    public void setnMatricula(int nMatricula) {
+        this.nMatricula = nMatricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
